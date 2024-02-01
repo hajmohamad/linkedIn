14defb3723140ddbbfd85990a2b2239519b457e1
ehsansaberi04 <126385651+ehsansaberi04@users.noreply.github.com>
2024-02-01 18:18:23 +0330

    delete fake json

--- linkedIn/src/main/java/com/example/linkedin/linkedIn.java
+++ linkedIn/src/main/java/com/example/linkedin/linkedIn.java
@@ -6,7 +6,12 @@ import javafx.scene.Scene;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.util.Iterator;
 
 import org.json.simple.JSONObject;
 import org.json.simple.JSONArray;
@@ -23,41 +28,9 @@ public class linkedIn extends Application {
         stage.initStyle(StageStyle.TRANSPARENT);
         scene.setFill(Color.TRANSPARENT);
         stage.show();
+        mainStage = stage ;
     }
-
     public static void main(String[] args) {
-//        launch();
-        JSONParser parser = new JSONParser();
-
-//        String s = "[0,{\"1\":{\"2\":{\"3\":{\"4\":[5,{\"6\":7}]}}}}]";
-
-        String s = linkedIn.class.getResource("users.json").toString() ;
-
-        try{
-            Object obj = parser.parse(s);
-            JSONArray array = (JSONArray)obj;
-            System.out.println("The 2nd element of array");
-            System.out.println(array.get(1));
-            System.out.println();
-            JSONObject obj2 = (JSONObject)array.get(1);
-            System.out.println("Field \"1\"");
-            System.out.println(obj2.get("1"));
-            s = "{}";
-            obj = parser.parse(s);
-            System.out.println(obj);
-            s = "[5,]";
-            obj = parser.parse(s);
-            System.out.println(obj);
-            s = "[5,,2]";
-            obj = parser.parse(s);
-            System.out.println(obj);
-        }catch(ParseException pe){
-            System.out.println("position: " + pe.getPosition());
-            System.out.println(pe);
-        }
-
-
-//---------------------------
-
+        launch();
     }
 }
